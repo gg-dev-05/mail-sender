c29ba3582f31f40261004c316161678e97019be6
= <garvitgalgat@gmail.com>
2020-12-26 05:01:38 +0530

    mail sent

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+require('dotenv').config();
+// console.log(require('dotenv').config({ debug: true }))
 
-
-
+const express = require('express')
 const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-    to: 'garvitgalgat@gmail.com', // Change to your recipient
-    from: 'test@example.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-    .send(msg)
-    .then(() => {
-        console.log('Email sent')
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+
+app.route('/').get((req, res) => {
+    res.send("HELLO WORLD")
+})
+
+app.route('/send').post((req, res) => {
+
+    console.log(req.body)
+    req.body.message += "\nFrom: " + req.body.name + " | " + req.body.from
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+        to: 'garvitgalgat@gmail.com',
+        from: 'cse190001016@iiti.ac.in', // Change to your verified sender
+        subject: req.body.subject,
+        text: req.body.message,
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            res.send('Email sent')
+        })
+        .catch((error) => {
+            res.send(error)
+        })
+
+})
+
+
+app.listen(5000, () => console.log('Server running on port 5000'))
